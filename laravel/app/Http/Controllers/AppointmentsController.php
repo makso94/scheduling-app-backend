00157dbe2c0bbb4cec89f51455aac9c01f950382
Mario Maksimovic <maki.makso@gmail.com>
2021-01-29 01:16:09 +0100

    Improved getAppointmentsByDate to additionaly return services for each appointment

--- laravel/app/Http/Controllers/AppointmentsController.php
+++ laravel/app/Http/Controllers/AppointmentsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\AppointmentsResource;
 use App\Models\Appointments;
+use App\Models\Service;
 use Exception;
 use Illuminate\Http\Request;
 
@@ -12,11 +13,15 @@ class AppointmentsController extends Controller
     public function getAppoitmentsByDate(Request $req)
     {
         try {
-            return new AppointmentsResource(
-                Appointments::where('working_days_id', $req->get('working_days_id'))
-                    ->orderBy('start')
-                    ->get()
-            );
+            // return new AppointmentsResource(
+            //     Appointments::where('working_days_id', $req->get('working_days_id'))
+            //         ->orderBy('start')
+            //         ->get()
+            // );
+            return new AppointmentsResource(Appointments::where('working_days_id', $req->get('working_days_id'))
+                ->with('services')
+                ->orderBy('start')
+                ->get());
         } catch (Exception $e) {
             return response()->json(['msg' => 'The given data was invalid'], 422);
         }
@@ -32,17 +37,23 @@ class AppointmentsController extends Controller
     }
     public function create(Request $req)
     {
-        $req->validate([
-            'working_day_id' => 'required',
-            'user_id' => 'required',
-            'start' => 'required',
+        try {
+            $req->validate([
+                'working_day_id' => 'required',
+                'user_id' => 'required',
+                'start' => 'required',
+                'service_ids' => 'required'
+            ]);
+            $app = new Appointments();
+            $app->working_days_id = $req->working_day_id;
+            $app->users_id = $req->user_id;
+            $app->start = $req->start;
+            $app->save();
 
-        ]);
-        // return $req;
-        $app = new Appointments();
-        $app->working_days_id = $req->working_day_id;
-        $app->users_id = $req->user_id;
-        $app->start = $req->start;
-        $app->save();
+            $app->services()->attach($req->service_ids);
+            return response()->json(['msg' => 'Successfuly created appointment'], 200);
+        } catch (Exception $e) {
+            return response()->json(['msg' => 'The given data was invalid'], 422);
+        }
     }
 }
--- laravel/app/Models/Service.php
+++ laravel/app/Models/Service.php
@@ -11,7 +11,7 @@ class Service extends Model
 
     protected $table = 'services';
 
-
+    protected $hidden = ['created_at', 'updated_at', 'pivot'];
 
     public function appointments()
     {
